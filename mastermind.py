46d0ec73cbed7de2b5ddd6950d9d192e8a81a2e3
ToMMoras <tommoras93@gmail.com>
2025-09-17 14:00:18 +0200

    ajout masterminf

--- mastermind.py
+++ mastermind.py
@@ -1,8 +1,8 @@
-fhndf
-h
-rt
-nhr
-tyrtnhrt
-hn
-r
-tnhrt
+import random
+
+COULEURS = ["bleu", "violet", "vert", "blanc", "jaune", "orange", "noir", "rouge"] 
+
+code_secret = []
+for i in range(6):  
+    couleur = random.choice(COULEURS)  
+    code_secret.append(couleur)
