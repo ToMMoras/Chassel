56c2c1c387d8b9186fca192bf187ed4fe5593ab8
ToMMoras <tommoras93@gmail.com>
2025-09-17 14:40:41 +0200

    mastermiiiiiiiiiiiiiiiinf

--- mastermind.py
+++
@@ -1,8 +0,0 @@
-import random
-
-COULEURS = ["bleu", "violet", "vert", "blanc", "jaune", "orange", "noir", "rouge"] 
-
-code_secret = []
-for i in range(6):  
-    couleur = random.choice(COULEURS)  
-    code_secret.append(couleur)
---
+++ masterminf.py
@@ -0,0 +1,32 @@
+import random
+
+COULEURS = ["R", "V", "B", "J", "M", "N"] 
+
+code_secret = []
+for i in range(6):  
+    couleur = random.choice(COULEURS)  
+    code_secret.append(couleur)
+
+saisie = input("Essai : ")
+proposition = list(saisie)
+
+bien_places = 0 
+for i in range(6):
+    if proposition[i] == code_secret[i]:
+        bien_places +=1
+
+secret_restant = []
+proposition_restante = []
+
+for i in range (6):
+    if proposition[i] != code_secret[i]:
+        secret_restant.append(code_secret[i])
+        proposition_restante.append(proposition[i])
+
+mal_places = 0 
+for couleur in proposition_restante:
+    if couleur in secret_restant:
+        mal_places += 1 
+        secret_restant.remove(couleur)
+
+print("Bien placés :", bien_places, "Mal placés :", mal_places)
