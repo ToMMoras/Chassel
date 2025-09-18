a0c7e558db52b1fa8c4e2bfad90334a93d54717b
ToMMoras <tommoras93@gmail.com>
2025-09-18 11:44:04 +0200

    Changment du nombre de lettre

--- Polytech-Dijon-Soutien/Mastermind_MVP.py
+++ Polytech-Dijon-Soutien/Mastermind_MVP.py
@@ -1,34 +1,33 @@
-#Test
 import random
 
 COULEURS = ["R", "V", "B", "J", "M", "N"] ##changer les lettres comme bon vous semble
 essai = 1
 code_secret = []
 
-for i in range(6):                         ##changer le nombre d'inconnu comme bon vous semble
+for i in range(4):                         ##changer le nombre d'inconnu comme bon vous semble
     couleur = random.choice(COULEURS)  
     code_secret.append(couleur)
 
 bien_places = 0 
 
-while essai <= 12 and bien_places <6:      ##changer le nombre d'essai comme bon vous semble ##changer le nombre d'inconnu en fonction du choix réaliser ligne 7
+while essai <= 12 and bien_places <4:      ##changer le nombre d'essai comme bon vous semble ##changer le nombre d'inconnu en fonction du choix réaliser ligne 7
     saisie = input("Essai : ")
     proposition = list(saisie)
 
-    if len(proposition) != 6:
-        print("vous devez impérativement mettre 6 lettre présentes parmi : R, V, B, J, M, N")
+    if len(proposition) != 4:           ##changer le nombre d'inconnu en fonction du choix réaliser ligne 7
+        print("vous devez impérativement mettre 4 lettre présentes parmi : R, V, B, J, M, N")
         continue
 
     bien_places = 0
 
-    for i in range(6):                      ##changer le nombre d'inconnu en fonction du choix réaliser ligne 7
+    for i in range(4):                      ##changer le nombre d'inconnu en fonction du choix réaliser ligne 7
         if proposition[i] == code_secret[i]:
             bien_places += 1
 
     secret_restant = []
     proposition_restante = []
 
-    for i in range (6):                     ##changer le nombre d'inconnu en fonction du choix réaliser ligne 7
+    for i in range (4):                     ##changer le nombre d'inconnu en fonction du choix réaliser ligne 7
         if proposition[i] != code_secret[i]:
             secret_restant.append(code_secret[i])
             proposition_restante.append(proposition[i])
@@ -40,9 +39,9 @@ while essai <= 12 and bien_places <6:      ##changer le nombre d'essai comme bon
             secret_restant.remove(couleur)
 
     essai += 1
-    print("Bien placés :", bien_places, "Mal placés :", mal_places)
+    print("Bien placés :", bien_places, " / Mal placés :", mal_places, " / nombre d'essai sur 12 : ", essai)
 
-if bien_places == 6:                            ##changer le nombre d'inconnu en fonction du choix réaliser ligne 7
+if bien_places == 4:                            ##changer le nombre d'inconnu en fonction du choix réaliser ligne 7
     print("Bravo c'est win")
 
 else:
